c1893d87e01c3cc6048a54a747e2e6d43bb48f6d
Nghia Luu Quang <nghialq@timebird.org>
2021-09-29 14:39:06 +0700

    Create auth service and api

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const {ResponseData} = require('../helpers/response-data')
-const historyController = require('../controllers/history-controller')
 const authController = require('../controllers/auth-controller')
 
-router.get('/minute', historyController.listHistoryMinute)
-
-router.get('/hour', historyController.listHistoryHour)
-
-router.put('/refresh', authController.isAuth, historyController.refreshAllHistory)
+router.post('/login', authController.login)
 
 router.use('/', (req, res) => {
   return res.status(404).json(new ResponseData(false, "Not found").toJson())
